f504143b3010bc8e6ea06ebdcee691d52e60b942
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-09-26 19:09:23 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    
+    +3); // code readability should be high
+console.log("Anil");
+
+let name = "Anil"
+let age = 14
+let isLoggedIn = false
+
+let state
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof "anil");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
